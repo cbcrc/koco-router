d13f36ef595a873b0f877a062c246e1517f42d13
MAXIME SEGUIN <MAXIME.FORGET-SEGUIN@RADIO-CANADA.CA>
2015-02-25 10:44:29 -0500

    -

--- src/router.js
+++ src/router.js
@@ -27,15 +27,6 @@ define(['jquery', 'knockout-utilities', 'knockout', 'lodash', 'byroads', 'router
 
             self.navigating = new RouterEvent();
 
-
-            //TODO: ?
-            //Permet d'afficher un dialog si ?dialog=dialog_name
-            // self.currentRoute.subscribe(function(route) {
-            //     if (route.dialog) {
-            //         self.showDialog(route.dialog);
-            //     }
-            // });
-
             configureRouting(self);
 
             self.settings = {};
@@ -87,14 +78,6 @@ define(['jquery', 'knockout-utilities', 'knockout', 'lodash', 'byroads', 'router
         Router.prototype._getRegisteredPage = function(name) {
             return this._pages[name];
         };
-
-
-        //routeConfig
-        //priority
-        //pageName
-        //title
-        //params
-
 
         Router.prototype.addRoute = function(pattern, routeConfig) {
             var self = this;
@@ -250,10 +233,6 @@ define(['jquery', 'knockout-utilities', 'knockout', 'lodash', 'byroads', 'router
                     return dfd.promise();
                 }
 
-                //TODO: Supporter signedIn! (requireAuthenticiation fonctionnera seulement pour les routes indentiques
-                //même pattern exact - on filter sur route.authenticationRequired)
-                //var signedIn = false;
-
                 if (matchedRoute) {
 
                     var navigateInnerPromise = navigateInner(self, matchedRoute.route);
@@ -264,6 +243,7 @@ define(['jquery', 'knockout-utilities', 'knockout', 'lodash', 'byroads', 'router
 
                             matchedRoute.activationData = activationData;
                             matchedRoute.url = finalUrl;
+                            //TODO: Simplify interface of public matchedRoute (ex. create a simpler route from matchedRoute)
                             self.currentRoute(matchedRoute);
                             self.lastUrl = finalUrl;
                             self._setPageTitle(matchedRoute);
@@ -286,14 +266,6 @@ define(['jquery', 'knockout-utilities', 'knockout', 'lodash', 'byroads', 'router
                     return dfd.promise();
 
                 }
-
-
-                //TODO: On veut toujours faire ça?
-                // route.params.queryParams = queryParams;
-                // route.params.parsedQueryString = chrissRogersJQqueryDeparam(queryParams["?query_"], true);
-                // route.params.request = queryParams["request_"];
-                // route.params.queryString = queryParams["?query_"];
-
             } catch (err) {
                 dfd.reject(err);
                 return dfd.promise();
@@ -389,103 +361,6 @@ define(['jquery', 'knockout-utilities', 'knockout', 'lodash', 'byroads', 'router
 
             return componentConfig;
         }
-
-        //https://github.com/chrissrogers/jquery-deparam/blob/master/jquery-deparam.js
-        //TODO: On ne sert plus de cette fonction actuellement dans le router...
-        //NE devrait peut-être pas faire partie du core... 
-        //pourrait plus faire partie d'une livrairie utilitiaire
-        // function chrissRogersJQqueryDeparam(params, coerce) {
-        //     var obj = {},
-        //         coerce_types = {
-        //             'true': !0,
-        //             'false': !1,
-        //             'null': null
-        //         };
-
-        //     if (params) {
-        //         // Iterate over all name=value pairs.
-        //         $.each(params.replace(/\+/g, ' ').split('&'), function(j, v) {
-        //             var param = v.split('='),
-        //                 key = decodeURIComponent(param[0]),
-        //                 val,
-        //                 cur = obj,
-        //                 i = 0,
-        //                 // If key is more complex than 'foo', like 'a[]' or 'a[b][c]', split it
-        //                 // into its component parts.
-        //                 keys = key.split(']['),
-        //                 keys_last = keys.length - 1;
-
-        //             // If the first keys part contains [ and the last ends with ], then []
-        //             // are correctly balanced.
-        //             if (/\[/.test(keys[0]) && /\]$/.test(keys[keys_last])) {
-        //                 // Remove the trailing ] from the last keys part.
-        //                 keys[keys_last] = keys[keys_last].replace(/\]$/, '');
-
-        //                 // Split first keys part into two parts on the [ and add them back onto
-        //                 // the beginning of the keys array.
-        //                 keys = keys.shift().split('[').concat(keys);
-
-        //                 keys_last = keys.length - 1;
-        //             } else {
-        //                 // Basic 'foo' style key.
-        //                 keys_last = 0;
-        //             }
-
-        //             // Are we dealing with a name=value pair, or just a name?
-        //             if (param.length === 2) {
-        //                 val = decodeURIComponent(param[1]);
-
-        //                 // Coerce values.
-        //                 if (coerce) {
-        //                     val = val && !isNaN(val) ? +val // number
-        //                         : val === 'undefined' ? undefined // undefined
-        //                         : coerce_types[val] !== undefined ? coerce_types[val] // true, false, null
-        //                         : val; // string
-        //                 }
-
-        //                 if (keys_last) {
-        //                     // Complex key, build deep object structure based on a few rules:
-        //                     // * The 'cur' pointer starts at the object top-level.
-        //                     // * [] = array push (n is set to array length), [n] = array if n is 
-        //                     //   numeric, otherwise object.
-        //                     // * If at the last keys part, set the value.
-        //                     // * For each keys part, if the current level is undefined create an
-        //                     //   object or array based on the type of the next keys part.
-        //                     // * Move the 'cur' pointer to the next level.
-        //                     // * Rinse & repeat.
-        //                     for (; i <= keys_last; i++) {
-        //                         key = keys[i] === '' ? cur.length : keys[i];
-        //                         cur = cur[key] = i < keys_last ? cur[key] || (keys[i + 1] && isNaN(keys[i + 1]) ? {} : []) : val;
-        //                     }
-
-        //                 } else {
-        //                     // Simple key, even simpler rules, since only scalars and shallow
-        //                     // arrays are allowed.
-
-        //                     if ($.isArray(obj[key])) {
-        //                         // val is already an array, so push on the next value.
-        //                         obj[key].push(val);
-
-        //                     } else if (obj[key] !== undefined) {
-        //                         // val isn't an array, but since a second value has been specified,
-        //                         // convert val into an array.
-        //                         obj[key] = [obj[key], val];
-
-        //                     } else {
-        //                         // val is a scalar.
-        //                         obj[key] = val;
-        //                     }
-        //                 }
-
-        //             } else if (key) {
-        //                 // No value was defined, so set something meaningful.
-        //                 obj[key] = coerce ? undefined : '';
-        //             }
-        //         });
-        //     }
-
-        //     return obj;
-        // }
 
 
         return new Router();
